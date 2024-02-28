3313127c8251a452036e3b08ce2eaba654b63182
Abdulmalik-amh <mloqk21@gmail.com>
2024-02-28 23:00:30 +0300

    added sounds

--- MainProject2/Assets/DisplayModel.cs
+++ MainProject2/Assets/DisplayModel.cs
@@ -6,19 +6,21 @@ public class DisplayModel : MonoBehaviour
 {
     // Start is called before the first frame updat
     public GameObject gameObject;
+    public AudioSource audioSource;
+    public AudioClip  audioClip;
 
 
     public void Desplay()
     {
 
         gameObject.SetActive(true);
-
+        audioSource.PlayOneShot(audioClip);
     }
     public void Destroy()
     {
 
         gameObject.SetActive(false);
-
+        audioSource.Stop();
     }
 
 }
